d99de389d2e4f1ced543ac3024a75606825f3335
Ritesh Thorve <riteshthorve2004@gmail.com>
2025-03-19 10:25:03 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const navItems = [
     { name: 'Home', path: '/' },
-    { name: 'Resume', path: 'https://drive.google.com/file/d/1gpOTWYoKcbyu4SK9ZQpsmGWkFrzNoSbE/view' },
+    { name: 'Resume', path: 'https://drive.google.com/file/d/1bv_WJnqf6l5lzC-xL-MWu_R6ILhhfi_C/view' },
     { name: 'About', path: '/about' },
     { name: 'Service', path: '/services' },
     { name: 'Project', path: '/portfolio' },

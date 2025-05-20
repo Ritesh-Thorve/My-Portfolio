d5c0fd0c0b9e6f9395309b303993846b5cf46c05
Ritesh Thorve <riteshthorve2004@gmail.com>
2025-05-20 17:45:05 +0530

    Replace Portfolio component with Projects in Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import Hero from '../components/Hero';
 import About from '../components/About';
 import Services from '../components/Services';
-import Portfolio from '../components/Portfolio';
+import Projects from '../components/Projects';
 import Contact from '../components/Contact';
 
 export default function Home() {
@@ -10,7 +10,7 @@ export default function Home() {
       <Hero />
       <About />
       <Services />
-      <Portfolio />
+      <Projects />
       <Contact />
     </>
   );

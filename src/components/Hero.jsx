2808194f91044787209465afa8a30b4e12dfd95b
Ritesh Thorve <riteshthorve2004@gmail.com>
2025-07-18 13:28:14 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -73,7 +73,7 @@ export default function Hero() {
               </Link>
 
               <NavLink 
-                to="https://drive.google.com/file/d/1JaeQ4jlV8TZ8oUj8YSi3pWJtZxGf57FT/view?usp=sharing" 
+                to="https://drive.google.com/file/d/1aMnMuQcQL-NkEBWUNAeWaGJ034hSkRES/view?usp=sharing" 
                 target="_blank" 
                 rel="noopener noreferrer"
               >
